4716f49667d25035393f6d1d5074ff48452f417f
Dimitris155 <dimitrissamolis2001@gmail.com>
2023-06-02 13:59:43 +0300

    loading time

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 3000);
+    }, 2000);
 
     return () => clearTimeout(timer);
   }, []);
